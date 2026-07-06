3b7a6c803c64825931c1e5c4486a317e9f882998
Arthur Lesle <arth.lesle@gmail.com>
2026-07-06 16:25:18 +0200

    changes to main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-louise = "Hello, World!" 
+from scripts.BasicStats import datalen, moyenne
+from sklearn.datasets import make_blobs
+
+X, Y = make_blobs(n_samples=100, n_features=4, random_state=42)
+print(moyenne(X))
--- src/main.py
+++
@@ -1,5 +0,0 @@
-from scripts.BasicStats import moyenne
-from sklearn.datasets import make_blobs
-
-data = make_blobs(n_samples=100, centers=6, n_features=4, random_state=42)
-print(moyenne(data))
